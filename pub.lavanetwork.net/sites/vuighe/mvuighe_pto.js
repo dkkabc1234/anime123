192e4c568c55d121a7a723b68088b6e76df40a95
dkkabc1234 <106858983+dkkabc1234@users.noreply.github.com>
2022-06-06 23:44:42 +0700

    Add files via upload

---
+++ pub.lavanetwork.net/sites/vuighe/mvuighe_pto.js
@@ -0,0 +1,76 @@
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini|Opera Mobile|Kindle|Windows Phone|PSP|AvantGo|Atomic Web Browser|Blazer|Chrome Mobile|Dolphin|Dolfin|Doris|GO Browser|Jasmine|MicroB|Mobile Firefox|Mobile Safari|Mobile Silk|Motorola Internet Browser|NetFront|NineSky|Nokia Web Browser|Obigo|Openwave Mobile Browser|Palm Pre web browser|Polaris|PS Vita browser|Puffin|QQbrowser|SEMC Browser|Skyfire|Tear|TeaShark|UC Browser|uZard Web|wOSBrowser|Yandex.Browser/i.test(navigator.userAgent)) {
+	/* var temp = 0;
+	fetch('https://delivery.lavamedia.vn/weather/index.php?city_id=1581130')
+	.then(function(response) {
+		return response.text()
+	}).then(function(text) {
+		temp = parseFloat(text); */
+	/* if(parseFloat(temp) > 35)
+	{
+		var m3_u2 = 'https://delivery.lavamedia.vn/www/delivery/ajs.php'; var m3_r2 = Math.floor(Math.random() * 99999999999);
+		document.write("<script type=\'text\/javascript\' src=\'" + m3_u2 + "?zoneid=4307" + "&amp;cb=" + m3_r2 + "&amp;charset=UTF-8&amp;loc=" + escape(window.location) + "&amp;referer=" + escape(document.referrer) + "&context=" + escape(document.context) + "'><\/scr" + "ipt>");
+		//console.log(parseFloat(temp) + " > 10 ");
+	} 
+	else
+	{
+		console.log(parseFloat(temp)  + " < 10 ");
+		var m3_u2 = 'https://delivery.lavamedia.vn/www/delivery/ajs.php'; var m3_r2 = Math.floor(Math.random() * 99999999999);
+		document.write("<script type=\'text\/javascript\' src=\'" + m3_u2 + "?zoneid=2413" + "&amp;cb=" + m3_r2 + "&amp;charset=UTF-8&amp;loc=" + escape(window.location) + "&amp;referer=" + escape(document.referrer) + "&context=" + escape(document.context) + "'><\/scr" + "ipt>");
+	}  */
+	/* });
+
+	var callBanner = setInterval(function(){
+		if(temp > 0)
+		{
+			clearInterval(callBanner);
+			if(temp > 30)
+			{ */
+	/* var newIns = document.createElement("ins");
+	newIns.setAttribute("data-revive-zoneid", "4307");
+	newIns.setAttribute("data-revive-id", "a2ac509200724887a4e14ea7e92c272c");
+	document.body.appendChild(newIns);
+
+	var newScript = document.createElement("script");
+	newScript.setAttribute("src", "https://delivery.lavamedia.vn/www/delivery/asyncjs.php");
+	newScript.addEventListener("load", function(){
+		window.reviveAsync["a2ac509200724887a4e14ea7e92c272c"].apply(window.reviveAsync["a2ac509200724887a4e14ea7e92c272c"].detect());
+	});
+	document.body.appendChild(newScript); */
+	/* }
+	else
+	{ */
+	//var newIns = document.createElement("ins");
+	//newIns.setAttribute("data-revive-zoneid", "1579");
+	//newIns.setAttribute("data-revive-id", "a2ac509200724887a4e14ea7e92c272c");
+	//document.body.appendChild(newIns);
+
+	//var newScript = document.createElement("script");
+	//newScript.setAttribute("src", "https://delivery.lavamedia.vn/www/delivery/asyncjs.php");
+	/* newScript.addEventListener("load", function(){
+		window.reviveAsync["a2ac509200724887a4e14ea7e92c272c"].apply(window.reviveAsync["a2ac509200724887a4e14ea7e92c272c"].detect());
+	}); */
+	//document.body.appendChild(newScript);
+
+
+	var newIns = document.createElement("ins");
+	newIns.setAttribute("data-revive-zoneid", "1579");
+	newIns.setAttribute("data-revive-id", "a2ac509200724887a4e14ea7e92c272c");
+	document.body.appendChild(newIns);
+
+	var newScript = document.createElement("script");
+	newScript.setAttribute("src", "https://delivery.lavamedia.vn/www/delivery/asyncjs.php");
+	document.body.appendChild(newScript);
+	/* }
+}
+}, 1000);
+*/
+/* var newScript = document.createElement("script");
+	newScript.setAttribute("src", "https://asia.adform.net/adfscript/?bn=55474367");
+	document.body.appendChild(newScript); */
+
+}
+else {
+	console.log("Lava Digital - Messenger - The banner don’t  show in the PC .");
+}
+
+
---
+++ pub.lavanetwork.net/sites/vuighe/vuighe_balloon.js
@@ -0,0 +1,28 @@
+if(! /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini|Opera Mobile|Kindle|Windows Phone|PSP|AvantGo|Atomic Web Browser|Blazer|Chrome Mobile|Dolphin|Dolfin|Doris|GO Browser|Jasmine|MicroB|Mobile Firefox|Mobile Safari|Mobile Silk|Motorola Internet Browser|NetFront|NineSky|Nokia Web Browser|Obigo|Openwave Mobile Browser|Palm Pre web browser|Polaris|PS Vita browser|Puffin|QQbrowser|SEMC Browser|Skyfire|Tear|TeaShark|UC Browser|uZard Web|wOSBrowser|Yandex.Browser/i.test(navigator.userAgent) ) {
+	/* var m3_u = (location.protocol=='https:'?'https://admin.lavamedia.vn/www/delivery/ajs.php':'http://admin.lavamedia.vn/www/delivery/ajs.php');
+	var m3_r = Math.floor(Math.random()*99999999999);
+	if (!document.MAX_used) document.MAX_used = ',';
+	document.write ("<scr"+"ipt type='text/javascript' src='"+m3_u);
+	document.write('?zoneid=1902&pubname=vuighe.net&pubid=429');
+	document.write ('&amp;cb=' + m3_r);
+	if (document.MAX_used != ',') document.write ("&amp;exclude=" + document.MAX_used);
+	document.write (document.charset ? '&amp;charset='+document.charset : (document.characterSet ? '&amp;charset='+document.characterSet : ''));
+	document.write ("&amp;loc=" + escape(window.location));
+	if (document.referrer) document.write ("&amp;referer=" + escape(document.referrer));
+	if (document.context) document.write ("&context=" + escape(document.context));
+	if (document.mmm_fo) document.write ("&amp;mmm_fo=1");
+	document.write ("'><\/scr"+"ipt>"); */
+
+	var newIns = document.createElement("ins");
+    newIns.setAttribute("data-revive-zoneid", "1573");
+    newIns.setAttribute("data-revive-id", "a2ac509200724887a4e14ea7e92c272c");
+    document.body.appendChild(newIns);
+
+    var newScript = document.createElement("script");
+    newScript.setAttribute("src", "https://delivery.lavamedia.vn/www/delivery/asyncjs.php");
+    document.body.appendChild(newScript);
+}
+else
+{
+	console.log("Lava Digital - Messenger - The banner don’t  show in the mobile device.");
+}
